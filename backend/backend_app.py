ddc9062037cbddd205afadf16232ca4da88d8109
Dmitriy Lubchenko <ludmitr87@gmail.com>
2023-06-22 19:58:26 +0300

    progress update

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -3,9 +3,11 @@ from flask_cors import CORS
 import logging
 from werkzeug.exceptions import BadRequest
 
-
 app = Flask(__name__)
 CORS(app)  # This will enable CORS for all routes
+
+ERROR_MSG_ID_NOT_FOUND = {"error": "id not found"}
+ERROR_MSG_DECODE_JSON = {"error": "Failed to decode JSON object"}
 
 # Configure logging
 logging.basicConfig(level=logging.INFO,
@@ -31,10 +33,10 @@ def manage_posts():
             and status code 400 if 'title' or 'content' are missing.
     """
     if request.method == 'POST':
-        # case when json was not passed
+        # case when json was not passed/ failed to decode json
         new_post_data = get_validated_json()
         if new_post_data is None:
-            return jsonify({"error": "Failed to decode JSON object"}), 400
+            return jsonify(ERROR_MSG_DECODE_JSON), 400
 
         # Validate if all required keys are present and have a value
         missing_keys = [key for key in ['title', 'content'] if not new_post_data.get(key)]
@@ -67,7 +69,7 @@ def delete_post(post_id: str):
         post_id = int(post_id)
 
         # remove post by id (if exist) amd return json of deleted post
-        post_to_delete = next((post for post in POSTS if post['id'] == post_id), None)
+        post_to_delete = get_post_by_id(post_id)
         if post_to_delete:
             POSTS.remove(post_to_delete)
             message_for_return = {
@@ -76,10 +78,45 @@ def delete_post(post_id: str):
             return jsonify(message_for_return), 200
 
     # case when id is not numeric or there is no post with that id
-    return jsonify({"error": "id not found"}), 404
+    return jsonify(ERROR_MSG_ID_NOT_FOUND), 404
+
+
+@app.route("/api/posts/<post_id>", methods=['PUT'])
+def update_post(post_id: str):
+    # case when id is not numeric
+    if not post_id.isnumeric():
+        return jsonify(ERROR_MSG_ID_NOT_FOUND), 404
+
+    # case when failed to decode json data from client
+    data_from_user_to_update = get_validated_json()
+    if data_from_user_to_update is None:
+        return jsonify(ERROR_MSG_DECODE_JSON), 400
+
+    # case when id is numeric but there is no post with that id
+    post_to_update = get_post_by_id(int(post_id))
+    if not post_to_update:
+        return jsonify(ERROR_MSG_ID_NOT_FOUND), 404
+
+    # updating post
+    data_to_update = get_data_to_update(data_from_user_to_update)
+    for key, val in data_to_update.items():
+        post_to_update[key] = val
+
+    return jsonify(post_to_update), 200
 
 
 #  --- inner logic ------------------------------------------------------
+def get_data_to_update(data_for_update: dict) -> dict:
+    """Returns dictionary with elements that needed to be updated in post"""
+    data_for_update = {key: val for key, val in data_for_update.items()
+                       if key in ['content', 'title'] and val}
+    return data_for_update
+
+
+def get_post_by_id(post_id: int):
+    """Returns post with post_id, if not exist - returns None"""
+    return next((post for post in POSTS if post['id'] == post_id), None)
+
 def get_unique_id_for_post() -> int:
     # case for no posts
     if not POSTS:
@@ -102,4 +139,4 @@ def get_validated_json() -> dict:
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5002)
+    app.run(host="0.0.0.0", port=5002, debug=True)
