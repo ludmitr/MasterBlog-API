aebd2d89588e0e82f7962d56b23e38252ede778e
Dmitriy Lubchenko <ludmitr87@gmail.com>
2023-06-25 02:37:39 +0300

    added new author and date fields to the blog and updated functions to match new changes

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 import logging
 from werkzeug.exceptions import BadRequest
+import datetime
 
 app = Flask(__name__)
 CORS(app)  # This will enable CORS for all routes
@@ -12,17 +13,30 @@ ERROR_MSG_DECODE_JSON = {"error": "Failed to decode JSON object"}
 # Configure logging
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(levelname)s: %(message)s',
-                    datefmt='%Y-%m-%d %H:%M:%S')
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    filename='combined_list.log')
 
 
 POSTS = [
-    {"id": 1, "title": "First post", "content": "This is the first post."},
-    {"id": 2, "title": "Second post", "content": "This is the second post."},
+    {"id": 1, "title": "First post", "content": "This is the first post.",
+     "author": "Your Name", "date": "2023-06-07"},
+    {"id": 2, "title": "Second post", "content": "This is the second post.",
+     "author": "Your Name", "date": "2023-06-07"},
 ]
 
 #  --- Routes ------------------------------------------------------
 @app.route('/api/posts', methods=['GET'])
 def get_posts():
+    """Return different result based on
+
+    - No query args passed - will return json with posts as it is.
+    - With passed args - will return sorted list of posts.
+
+        Query Parameters (both must be present):
+        - sort : Attribute of the post to sort by. Acceptable values are "title" or "content" or... .
+        - direction : Direction of the sort. Acceptable values are "asc" (ascending) or "desc" (descending).
+
+    """
     # case when no args passed. return posts as it is.
     if not request.args:
         return jsonify(POSTS), 200
@@ -31,13 +45,20 @@ def get_posts():
     direction_criteria = request.args.get('direction')
 
     # case when passed arguments are validated
-    if sort_criteria in ["title", "content"] and direction_criteria in ["asc", "desc"]:
+    if is_query_args_are_valid(sort_criteria, direction_criteria):
         is_reverse = True if direction_criteria == 'desc' else False
-        sorted_posts = sorted(POSTS, key=lambda post: post[sort_criteria], reverse=is_reverse)
+
+        # sorting. different sort for value of key 'date'
+        if sort_criteria == "date":
+            sorted_posts = sorted(POSTS,
+                                  key=lambda post: datetime.datetime.strptime(post[sort_criteria], '%Y-%m-%d'),
+                                  reverse=is_reverse)
+        else:
+            sorted_posts = sorted(POSTS, key=lambda post: post[sort_criteria], reverse=is_reverse)
 
         return jsonify(sorted_posts), 200
 
-    # case for passing wrong arguments
+    # ---- case for passing wrong query arguments-----
     error_messages = []
 
     # check sort_criteria
@@ -54,32 +75,37 @@ def get_posts():
         error_messages.append(
             "Direction argument value wrong, should be 'asc' or 'desc'.")
 
-    return jsonify({"error":f"{' ||| '.join(error_messages)}"}), 400
+    return jsonify({"error": f"{' ||| '.join(error_messages)}"}), 400
+
 
 @app.route('/api/posts', methods=['POST'])
 def add_post():
     """
     Handle POST requests to the '/api/posts' route.
     - POST: Adds a new post using JSON data from the request body.
-            The request body must contain 'title' and 'content' keys.
             Returns JSON with  added post with a unique 'id', or an error message
             and status code 400 if 'title' or 'content' are missing.
     """
-    # case when json was not passed/ failed to decode json
+    # case when json was not passed / failed to decode json
     new_post_data = get_validated_json()
     if new_post_data is None:
         return jsonify(ERROR_MSG_DECODE_JSON), 400
 
     # Validate if all required keys are present and have a value
-    missing_keys = [key for key in ['title', 'content'] if not new_post_data.get(key)]
+    missing_keys = [key for key in ['title', 'content', 'author', 'date'] if not new_post_data.get(key)]
     if missing_keys:
         return jsonify({'error': f"Missing or empty key(s): {missing_keys}"}), 400
+    if not is_valid_date_format(new_post_data.get('date')):
+        return jsonify(
+            {'error': f"date format should be: YYYY-MM-DD"}), 400
 
     # creating new post and adding to db
     new_post = {
         'id': get_unique_id_for_post(),
         'content': new_post_data['content'],
-        'title': new_post_data['title']}
+        'title': new_post_data['title'],
+        'author': new_post_data['author'],
+        'date': new_post_data['date']}
     POSTS.append(new_post)
 
     return jsonify(new_post), 201
@@ -137,39 +163,47 @@ def update_post(post_id: str):
 
 @app.route('/api/posts/search', methods=['GET'])
 def search_posts():
-    title_to_search = request.args.get('title')
-    content_to_search = request.args.get('content')
+    search_params = {
+        'title': request.args.get('title'),
+        'content': request.args.get('content'),
+        'author': request.args.get('author'),
+        'date': request.args.get('date')
+    }
 
-    title_search_result = search_posts_by_word_in_title(title_to_search)
-    content_to_search = search_posts_by_word_in_content(content_to_search)
+    search_result = []
+    for search_key, search_word in search_params.items():
+        if search_params[search_key]:
+            search_result.extend(search_posts_by_word(search_word, search_key))
 
-    combined_result: list = title_search_result + content_to_search
+    # combined_result: list = title_search_result + content_to_search
 
     # removing duplicates
-    combined_result = [post for index, post in enumerate(combined_result)
-                       if combined_result.index(post) == index]
+    combined_result = [post for index, post in enumerate(search_result)
+                       if search_result.index(post) == index]
 
     return jsonify(combined_result), 200
 
 
 #  --- inner logic ------------------------------------------------------
-def search_posts_by_word_in_title(title_word_to_search: str) -> list[dict]:
-    """search posts where title got title_word_to_search"""
-    result_of_search = []
-    title_word_to_search = title_word_to_search.lower()
-    for post in POSTS:
-        split_lower_words_of_title: list = post['title'].lower().split()
-        if title_word_to_search in split_lower_words_of_title:
-            result_of_search.append(post)
+def is_valid_date_format(date_string):
+    try:
+        datetime.datetime.strptime(date_string, '%Y-%m-%d')
+        return True
+    except ValueError:
+        return False
+def is_query_args_are_valid(sort_criteria: str, direction_criteria: str) -> bool:
+    return sort_criteria in ["title", "content", "author", "date"] \
+        and direction_criteria in ["asc", "desc"]
 
-    return result_of_search
 
-def search_posts_by_word_in_content(content_word_to_search: str) -> list[dict]:
-    """search posts where content got content_word_to_search"""
+def search_posts_by_word(word_to_search: str, key_to_search: str):
+    """search posts where word_to_search is matched to one of the words in value
+    of the key_to_search"""
+
     result_of_search = []
-    title_word_to_search = content_word_to_search.lower()
+    title_word_to_search = word_to_search.lower()
     for post in POSTS:
-        split_lower_words_of_title: list = post['content'].lower().split()
+        split_lower_words_of_title: list = post[key_to_search].lower().split()
         if title_word_to_search in split_lower_words_of_title:
             result_of_search.append(post)
 
@@ -179,7 +213,7 @@ def search_posts_by_word_in_content(content_word_to_search: str) -> list[dict]:
 def get_data_to_update(data_for_update: dict) -> dict:
     """Returns dictionary with elements that needed to be updated in post"""
     data_for_update = {key: val for key, val in data_for_update.items()
-                       if key in ['content', 'title'] and val}
+                       if key in ['content', 'title', 'author', 'date'] and val}
     return data_for_update
 
 
